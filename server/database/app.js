24d36712a0807643e40d1645f8741de610bc6c3e
r4jibooqis <raji.makkah@gmail.com>
2024-07-30 01:44:21 -0400

    fixing workflow v2

--- server/database/app.js
+++ server/database/app.js
@@ -8,7 +8,7 @@ const  cors = require('cors');
 const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -93,7 +93,7 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } );
-  let new_id = documents[0]['id']+1;
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 		"id": new_id,
